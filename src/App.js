3c3df3f764f5fb040016c106810b94367726fa99
gshanbhag525 <17478096+gshanbhag525@users.noreply.github.com>
2021-07-22 19:03:31 +0530

    Class  components and state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      string: 'Hello Gunesh'
+    };
+  }
+  
+  
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <button
+            onClick={ () => this.setState({string: 'Hello Kartik'}) }> 
+            Hi there CLickme
+          </button>
+          <p>{this.state.string}</p>
+
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;

64c36a27b7f51ee8a2a6d3ddff51e0078de14f7a
gshanbhag525 <17478096+gshanbhag525@users.noreply.github.com>
2021-07-24 17:54:20 +0530

    added props children

--- src/components/card-list/card-list-component.jsx
+++ src/components/card-list/card-list-component.jsx
@@ -3,5 +3,5 @@ import React from 'react';
 // () is a functional component and we can pass props to it.
 export const CardList = props => {
     console.log(props);
-    return (<div>Hello</div>); 
+    return <div>{props.children}</div>; 
 }

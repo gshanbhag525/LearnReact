8c6cc55d3bb0d96eeb613722624a48761ae5371c
gshanbhag525 <17478096+gshanbhag525@users.noreply.github.com>
2021-08-05 20:51:15 +0530

    added background, changed search appearance and addedheading

--- src/App.js
+++ src/App.js
@@ -43,6 +43,7 @@ class App extends React. Component {
     // the monsters array and then updating the cardList
     return (
       <div className="App">
+        <h1 id="webPageHeading">Cats Yellow Pages</h1>
         <SearchBox 
           placeholder='search those who exist'
           handleChange={this.handleChange}
